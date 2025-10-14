d5d779b869a93f30148909d3a22e7cd17efc54ec
AbstractEndeavors <partners@abstractendeavors.com>
2025-10-14 18:00:24 -0500

    Deploy version 0.0.0.51 at 2025-10-14 23:00:24 UTC

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='abstract_react',
-    version='0.0.0.50',
+    version='0.0.0.51',
     author='putkoff',
     author_email='partners@abstractendeavors.com',
     description="",
